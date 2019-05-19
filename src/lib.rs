de2f34fffd7e704cac01be78ab9f3a815f75657f
Luca Barbato <lu_zero@gentoo.org>
2019-05-19 14:45:51 +0200

    Add some high level abstractions
    - Context wrapping pl_context
    - vulkan::Instance wrapping pl_vk_inst

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,54 @@
+use libplacebo_sys::*;
+
+pub struct Context {
+    ctx: *mut pl_context,
+}
+
+impl Default for Context {
+    fn default() -> Self {
+        let ctx = unsafe { pl_context_create(PL_API_VER as i32, &pl_context_default_params) };
+        assert!(!ctx.is_null());
+
+        Context { ctx }
+    }
+}
+
+impl Drop for Context {
+    fn drop(&mut self) {
+        unsafe {
+            let ctx = &mut self.ctx;
+            pl_context_destroy(ctx);
+        }
+    }
+}
+
+pub mod vulkan {
+    use super::*;
+
+    pub struct Instance {
+        inst: *const pl_vk_inst,
+    }
+
+    impl Instance {
+        pub fn new(ctx: Context) -> Self {
+            let inst = unsafe { pl_vk_inst_create(ctx.ctx, &pl_vk_inst_default_params) };
+            assert!(!inst.is_null());
+
+            Instance { inst }
+        }
+    }
+
+    impl Drop for Instance {
+        fn drop(&mut self) {
+            unsafe {
+                let inst = &mut self.inst;
+                pl_vk_inst_destroy(inst);
+            }
+        }
+    }
+
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
